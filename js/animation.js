3468db7cc36eac86e38523971d9a2a3757d31cec
coco204 <hiroko.tanaka.126@gmail.com>
2024-04-08 23:40:47 +0900

    animation-top

---
+++ js/animation.js
@@ -0,0 +1,40 @@
+// eachTextAnimeにappeartextというクラス名を付ける定義
+function waControl() {
+  $(".writing-animation").each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("appeartext");
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+}
+
+// load---after loding the screen
+$(window).on("load", function () {
+  //add span
+  var element = $(".writing-animation");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = "";
+    text.split("").forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox +=
+            '<span style="animation-delay:.' + i + 's;">' + t + "</span>";
+        } else {
+          var n = i / 10;
+          textbox +=
+            '<span style="animation-delay:' + n + 's;">' + t + "</span>";
+        }
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  waControl();
+});
