7c7cd7bdd01ae4d11cb725ad7da7c59ea0b9666b
coco204 <hiroko.tanaka.126@gmail.com>
2024-04-09 15:49:48 +0900

    test

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,6 @@
-// eachTextAnimeにappeartextというクラス名を付ける定義
+//--------------------------
+// writing-animation
+//----------------------------
 function waControl() {
   $(".writing-animation").each(function () {
     var elemPos = $(this).offset().top - 50;
@@ -37,4 +39,47 @@ $(window).on("load", function () {
   });
 
   waControl();
+});
+
+//--------------------------
+// 1p-scroll-animation
+//----------------------------
+$.scrollify({
+  section: "1p-scroll",
+  scrollbars: "false",
+  interstitialSection: "#header,#fv,#footer",
+  easing: "swing",
+  scrollSpeed: 300,
+
+  //page-nation
+  before: function (i, panels) {
+    var ref = panels[i].attr("data-section-name");
+    $(".pagination .active").removeClass("active");
+    $(".pagination")
+      .find('a[href="#' + ref + '"]')
+      .addClass("active");
+  },
+  afterRender: function () {
+    var pagination = '<ul class="pagination">';
+    var activeClass = "";
+    $(".1p-scroll").each(function (i) {
+      activeClass = "";
+      if (i === $.scrollify.currentIndex()) {
+        activeClass = "active";
+      }
+      pagination +=
+        '<li><a class="' +
+        activeClass +
+        '" href="#' +
+        $(this).attr("data-section-name") +
+        '"><span class="hover-text">' +
+        $(this).attr("data-section-name").charAt(0).toUpperCase() +
+        $(this).attr("data-section-name").slice(1) +
+        "</span></a></li>";
+    });
+    pagination += "</ul>";
+
+    $("#scroll-1").append(pagination); //はじめのエリアにページネーションを表示
+    $(".pagination a").on("click", $.scrollify.move);
+  },
 });
